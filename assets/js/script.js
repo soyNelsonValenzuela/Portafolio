15b74bf8bb549bfd6f70ce54228bc0c1e091f0c5
soyNelsonValenzuela <nelsonvalenzuelagomez@gmail.com>
2024-08-29 13:11:26 -0400

    agrega redes sociales a scrollFunction

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,46 +4,55 @@ let skillItem = document.querySelectorAll('.skill-item')
 //Oculta menu colapsable cuando seleccionamos algun item en la version mobile
 
 navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        navItems.forEach(i => {
-            i.classList.remove('active')
-        })
-        item.classList.add('active')
-        document.querySelector('#navbarSupportedContent').classList.remove("show")
+  item.addEventListener('click', () => {
+    navItems.forEach(i => {
+      i.classList.remove('active')
     })
+    item.classList.add('active')
+    document.querySelector('#navbarSupportedContent').classList.remove("show")
+  })
 })
 
-//Colorea las skill al hacer hover y las vuelve a escala de grices al hacer mouse out 
+//Colorea las skill al hacer hover y las vuelve a escala de grices al hacer mouse out si es que la patanlla tiene un ancho minimo de 1024px
+
 skillItem.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.style.filter = 'grayscale(0)';
-        item.style.border = '1px solid #6BD1FF'
-        item.style.boxShadow = '0px 0px 30px #6BD1FF'
-    })
-    item.addEventListener('mouseout', () => {
-        item.style.filter = 'grayscale(100%)'
-        item.style.border = 'none'
-        item.style.boxShadow = '2px 2px 10px black'
-    })
+  item.addEventListener('mouseover', () => {
+    item.style.filter = 'grayscale(0)';
+    item.style.border = '1px solid #6BD1FF'
+    item.style.boxShadow = '0px 0px 30px #6BD1FF'
+  })
+  item.addEventListener('mouseout', () => {
+    document.body.clientWidth > 1024 ? item.style.filter = 'grayscale(100%)' : 'grayscale(0%)'
+    item.style.border = 'none'
+    item.style.boxShadow = '2px 2px 10px black'
+  })
 })
 
+// Get the buttons:
 
-// Get the button:
-let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+let topButton = document.getElementById("myBtn");
+let socialSharing = document.getElementById('social-sharing-float');
+
+
+
+// When the user scrolls down 500px from the top of the document, show the buttons
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "flex";
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    topButton.style.display = "flex";
+    if (document.body.clientWidth > 1024) {
+      socialSharing.style.display = "block"
+    }
   } else {
-    mybutton.style.display = "none";
+    topButton.style.display = "none";
+    socialSharing.style.display = "none";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+topButton.addEventListener('click', ()=> {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+})
