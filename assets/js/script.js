279d6193a18b1a6eb210ef3a058f1db127942913
soyNelsonValenzuela <nelsonvalenzuelagomez@gmail.com>
2024-08-29 11:42:42 -0400

    agrega logica boton subir

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let navItems = document.querySelectorAll('.nav-link')
 let skillItem = document.querySelectorAll('.skill-item')
 
+//Oculta menu colapsable cuando seleccionamos algun item en la version mobile
 
 navItems.forEach(item => {
     item.addEventListener('click', () => {
@@ -12,6 +13,7 @@ navItems.forEach(item => {
     })
 })
 
+//Colorea las skill al hacer hover y las vuelve a escala de grices al hacer mouse out 
 skillItem.forEach(item => {
     item.addEventListener('mouseover', () => {
         item.style.filter = 'grayscale(0)';
@@ -25,3 +27,23 @@ skillItem.forEach(item => {
     })
 })
 
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
