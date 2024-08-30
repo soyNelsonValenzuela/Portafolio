0d5a9c994d59aa6a4946ad8bf538632ae8734fa2
soyNelsonValenzuela <nelsonvalenzuelagomez@gmail.com>
2024-08-29 22:23:13 -0400

    agrega customErrors

---
+++ assets/js/customErrors.js
@@ -0,0 +1,23 @@
+export const tiposDeError = [
+    "valueMissing", "typeMismatch", "patternMissMatch" , "tooShort"
+]
+
+export const mensajes = {
+    nombre: {
+      valueMissing: "El campo nombre no puede estar vacío.",
+      patternMissmatch: "Por favor, ingrese un nombre válido.",
+      tooShort: "El campo no tiene caracteres suficientes.",
+    },
+    correo: {
+      valueMissing: "El campo email no puede estar vacío.",
+      typeMismatch: "Por favor, ingrese un email válido.",
+      tooShort: "El campo no tiene caracteres suficientes.",
+    },
+    asunto: {
+      valueMissing: "El campo asunto no puede estar vacío.",
+    },
+    mensajeUsuario: {
+      valueMissing: "El campo mensaje no puede estar vacío.",
+      tooShort: "El campo no tiene caracteres suficientes.",
+    }
+  };
